05c5025ff6a2e68f24f653aeefbf36afdb10053a
Kristin <kahuber@asu.edu>
2020-08-05 19:47:15 -0700

    Update of pixelData
    Change in pixelData to include different variables than perviously considered.

--- src/Furi/pixelData.java
+++ src/Furi/pixelData.java
@@ -2,20 +2,13 @@ package Furi;
 
 public class pixelData {
 	// x value, y value, r, g, b, h, s, br, %signal
-	public int x, y, r, g, b; 
-	public float h, s, br; 
+	public String filename, foldername; 
+	public double dblTotalPixels, coloredpixels, stainedpixels; 
 	public double signal; 
 	
 	public pixelData() {
 		// TODO Auto-generated constructor stub
-		x = 0; 
-		y = 0;
-		r = 0; 
-		g = 0; 
-		b = 0;
-		h = 0; 
-		s = 0;
-		br = 0; 
+		
 		signal = 0.0; 
 		
 	}
