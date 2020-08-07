3760f8a4c38a881d3eb9922314dc49f82db5961b
kswizzle67 <44689815+kswizzle67@users.noreply.github.com>
2020-08-06 18:38:39 -0700

    Updated Export code.

--- src/Furi/exportData.java
+++ src/Furi/exportData.java
@@ -12,16 +12,7 @@ public class exportData {
 	
 	public static void exporttoCSV(){
 		
-		// listener? for window where they can type file name. 
-		// and it sets it equal to namecsv
-		
-		
-		// want a button to say "save to excel" and it saves current slider bar value.
-		// https://examples.javacodegeeks.com/core-java/writeread-csv-files-in-java-example/
-		// I don't know why type final won't work. 
-		 
-		
-	
+
 	}
 	
 	public static void exporttocsvfile(String csvfile, ArrayList<pixelData> DtoS) {
@@ -29,47 +20,31 @@ public class exportData {
 		String comma = ","; 
 		String separator = "\n"; 
 		
-		String header = "X, Y, r, g, b, H, S, B, Percent Area";
-		
-		
-		
-		// x value, y value, r, g, b, h, s, b, %signal
-		
-		//calculate percent area and add it as last element
-		// we need total number of pixels that fall within accepted
-		// range divided by total pixels in image. Doesn't
-		// matter that image was scaled down. 
-		
-		
+		String header = "FileName, FolderName, TotalPixels, ColoredPixels, StainedPixels, Signal";
 		
 		FileWriter fileWriter = null; 
 			try {
 			
-		fileWriter = new FileWriter(csvfile);
+			fileWriter = new FileWriter(csvfile);
 			fileWriter.append(header.toString());
 			fileWriter.append(separator); 
 			
 			for(pixelData pd : DtoS) {
-				fileWriter.append(Integer.toString(pd.x));
+				fileWriter.append(pd.filename);
+				fileWriter.append(comma);
+				fileWriter.append(pd.foldername);
+				fileWriter.append(comma);
+				fileWriter.append(Double.toString(pd.dblTotalPixels));
+				fileWriter.append(comma);
+				fileWriter.append(Double.toString(pd.coloredpixels));
+				fileWriter.append(comma);
+				fileWriter.append(Double.toString(pd.stainedpixels));
+				fileWriter.append(comma);
+				fileWriter.append(Double.toString(pd.signal));
 				fileWriter.append(separator);
-				fileWriter.append(Integer.toString(pd.y));
-				fileWriter.append(separator);
-				fileWriter.append(Integer.toString(pd.r));
-				fileWriter.append(separator);
-				fileWriter.append(Integer.toString(pd.g));
-				fileWriter.append(separator);
-				fileWriter.append(Integer.toString(pd.b));
-				fileWriter.append(separator);
-				fileWriter.append(Float.toString(pd.h));
-				fileWriter.append(separator);
-				fileWriter.append(Float.toString(pd.s));
-				fileWriter.append(separator);
-				fileWriter.append(Float.toString(pd.br));
-				fileWriter.append(separator);
-				
-				
-				
 			}
+			fileWriter.close();
+			
 			System.out.println("CSV successful!"); 
 		}
 		catch (Exception e) {
